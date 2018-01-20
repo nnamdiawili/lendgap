0845760b113e24ecbf8e7e3b810c9138e005038f
Awili Uzo <uzo.awili@andela.com>
2018-01-20 19:25:27 +0100

    completes grid component and the landing page business and benefits sections

--- js/script.js
+++ js/script.js
@@ -1,55 +1,58 @@
+(function (window, document) {
 
-(function(window, document) {
-
-    var hero = document.querySelector('.hero');
-    var heroTitle = hero.querySelector('.hero .title');
-    var heroGradient = document.querySelector('.hero .gradient');
-    var navbar = document.querySelector('.navbar');
-    var overlays = document.querySelectorAll('.overlays');
+  var hero = document.querySelector('.hero');
+  var heroTitle = hero.querySelector('.hero .title');
+  var heroGradient = document.querySelector('.hero .gradient');
+  var navbar = document.querySelector('.lgp-navbar');
+  var bookmarksToggleInput = document.querySelector('#bookmarks-toggle-input');
 
 
-    var scrollTriggerOffset = 100, // offset to trigger scroll effects before the scroll position is reached.
-        heroTitleScrollOffset;
+  var scrollTriggerOffset = 100, // offset to trigger scroll effects before the scroll position is reached.
+      heroTitleScrollOffset;
 
-    function getScrollOffsets() {
-        heroTitleScrollOffset =  hero.offsetTop + heroTitle.offsetTop - scrollTriggerOffset;
+  function getScrollOffsets() {
+    heroTitleScrollOffset = hero.offsetTop + heroTitle.offsetTop - scrollTriggerOffset;
+  }
+
+  function handlePageScroll() {
+    if (window.pageYOffset > heroTitleScrollOffset) {
+      navbar.classList.add('pink-theme');
+      navbar.classList.add('condensed');
+    } else {
+      navbar.classList.remove('pink-theme');
+      navbar.classList.remove('condensed');
     }
-    function handlePageScroll() {
-        if (window.pageYOffset > heroTitleScrollOffset) {
-            navbar.classList.add('pink-theme');
-            navbar.classList.add('condensed');
-        } else {
-            navbar.classList.remove('pink-theme');
-            navbar.classList.remove('condensed');
-        }
-    }
-    function moveGradient(x, y, noTween) {
-      if (noTween) {
-          heroGradient.style.left = x;
-          heroGradient.style.top = y;
-      } else {
-          TweenMax.to(heroGradient, 0.6, {css: { left: x, top: y }});
-      }
-    }
-    function setOverlayEffects(x, y, noTween) {
-      if (noTween) {
-          heroGradient.style.left = x;
-          heroGradient.style.top = y;
-      } else {
-          TweenMax.to(heroGradient, 0.6, {css: { left: x, top: y }});
-      }
-    }
+  }
 
-    navbar.classList.add('fixed');
+  function moveGradient(x, y, noTween) {
+    if (noTween) {
+      heroGradient.style.left = x;
+      heroGradient.style.top = y;
+    } else {
+      TweenMax.to(heroGradient, 0.6, {css: {left: x, top: y}});
+    }
+  }
+
+  function handleMarketPlaceBookmarksToggle() {
+    bookmarksToggleInput.addEventListener('change', function () {
+      var nonBookmarks = document.querySelectorAll('.marketplace .loan-card:not(.bookmark)');
+      Array.prototype.forEach.call(nonBookmarks, function (nonBookmark) {
+        nonBookmark.closest('.grid-item').classList.toggle('hidden');
+      });
+    });
+  }
+
+  navbar.classList.add('fixed');
+  getScrollOffsets();
+  handlePageScroll();
+  handleMarketPlaceBookmarksToggle();
+  document.addEventListener('mousemove', function (e) {
+    moveGradient(e.clientX, e.clientY);
+  });
+  window.addEventListener('resize', function (e) {
     getScrollOffsets();
-    handlePageScroll();
-    document.addEventListener("mousemove", function(e) {
-      moveGradient(e.clientX, e.clientY);
-    });
-    window.addEventListener("resize", function(e) {
-      getScrollOffsets();
-      moveGradient('50%', '50%');
-    });
-    window.addEventListener('scroll', handlePageScroll);
+    moveGradient('50%', '50%');
+  });
+  window.addEventListener('scroll', handlePageScroll);
 
 })(window, document);
