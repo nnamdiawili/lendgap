f7541467150bccabd76b9bc1fcb5f1b40b8189c4
Awili Uzo <uzo.awili@andela.com>
2018-02-14 11:19:19 +0100

    add close icon to landing and implement loan bookmark toggling

--- app/js/global.js
+++ app/js/global.js
@@ -1,10 +1,15 @@
 (function (window, document) {
 
-  var bookmarksToggleInput = document.querySelector('#bookmarks-toggle-input');
-  var sidebarToggles = document.querySelectorAll('#bookmarks-toggle-input');
-
-
-  function handleMarketPlaceBookmarksToggle() {
+  function handleLoanBookmarkToggle() {
+    var loanBookmarkToggles = document.querySelectorAll('.loan-card .bookmark-toggle, .loan-modal .bookmark-toggle');
+    Array.prototype.forEach.call(loanBookmarkToggles, function (bookmarkToggle) {
+      bookmarkToggle.addEventListener('click', function (e) {
+        e.currentTarget.closest('.loan-card, .loan-modal').classList.toggle('bookmark');
+      });
+    });
+  }
+  function handleMarketPlaceBookmarksFilter() {
+    var bookmarksToggleInput = document.querySelector('#bookmarks-toggle-input');
     bookmarksToggleInput.addEventListener('change', function () {
       var nonBookmarks = document.querySelectorAll('.marketplace .loan-card:not(.bookmark)');
       Array.prototype.forEach.call(nonBookmarks, function (nonBookmark) {
@@ -24,13 +29,15 @@
       e.currentTarget.closest('.sidebar-container').classList.remove('sidebar-opened');
       e.currentTarget.removeEventListener('click', closeSidebar);
     }
+
     var sidebarToggles = document.querySelectorAll('.sidebar-toggle');
     Array.prototype.forEach.call(sidebarToggles, function (sidebarToggle) {
       sidebarToggle.addEventListener('click', openSidebar);
     });
   }
 
-  handleMarketPlaceBookmarksToggle();
+  handleMarketPlaceBookmarksFilter();
+  handleLoanBookmarkToggle();
   handleSidebarToggling();
 
 })(window, document);
