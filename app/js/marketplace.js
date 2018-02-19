bc18332f3b3b0e08d1da19dce56d98423561f9b5
Awili Uzo <uzo.awili@andela.com>
2018-02-19 09:51:23 +0100

    transactions dashboard view added

--- app/js/marketplace.js
+++ app/js/marketplace.js
@@ -1,17 +1,17 @@
 (function (window, document) {
 
   function handleLoanBookmarkToggle() {
-    var loanBookmarkToggles = document.querySelectorAll('.loan-card .bookmark-toggle, .loan-modal .bookmark-toggle');
+    var loanBookmarkToggles = document.querySelectorAll('.loan-grid-card .bookmark-toggle, .loan-modal .bookmark-toggle');
     Array.prototype.forEach.call(loanBookmarkToggles, function (bookmarkToggle) {
       bookmarkToggle.addEventListener('click', function (e) {
-        e.currentTarget.closest('.loan-card, .loan-modal').classList.toggle('bookmark');
+        e.currentTarget.closest('.loan-grid-card, .loan-modal').classList.toggle('bookmark');
       });
     });
   }
   function handleMarketPlaceBookmarksFilter() {
     var bookmarksToggleInput = document.querySelector('#bookmarks-toggle-input');
     bookmarksToggleInput.addEventListener('change', function () {
-      var nonBookmarks = document.querySelectorAll('.marketplace .loan-card:not(.bookmark)');
+      var nonBookmarks = document.querySelectorAll('.marketplace .loan-grid-card:not(.bookmark)');
       Array.prototype.forEach.call(nonBookmarks, function (nonBookmark) {
         nonBookmark.closest('.grid-item').classList.toggle('hidden');
       });
